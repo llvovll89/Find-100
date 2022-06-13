361d2b8e8009c3189d170ca1be9187ada4626fcc
llvovll89 <svvvs5579@naver.com>
2022-06-13 16:32:22 +0900

    create time

--- set.js
+++ set.js
@@ -1,8 +1,25 @@
+// time
+const today = document.querySelector('.today');
+function timeAdd(){
+const ab = new Date();
+const bc = ab.getHours();
+const cd = ab.getMinutes();
+const ma = ab.getSeconds();
+today.innerHTML = `${bc} 시 ${cd} 분 ${ma} 초`
+}
+
+const todayInterval = setInterval(timeAdd, 1000);
+// timer
+const mint = document.querySelector('.min');
+const secd = document.querySelector('.sec');
+
+// 100찾기 btn Event
 const btn = document.querySelector('.btn').addEventListener('click', () => {
     let a = document.querySelector('.random-num');
     let b = Math.floor(Math.random() * 100) + 1;
     a.innerText = b;
     if(b === 100){
         alert(`${b} 을 찾았어!!!`);
+        a.innerText = '100 찾았습니다!';
     }
 });
